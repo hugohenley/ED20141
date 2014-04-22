c717426f519ab1ecfbc906d2e784a1360a91de16
Hugo Henley <hugohenley@gmail.com>
2014-04-22 19:07:54 -0300

    exercicio 10

--- Resolucao_lista1/exercicio_10.c
+++ Resolucao_lista1/exercicio_10.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+/* TODO: Implementar condicionais e calculo de meses de trabalho! :) */
+
+
 int main (void) {
-	int mes_corrente, ano_corrente;
+	int mes_corrente, ano_corrente, i, matricula_atual;
+		
+	float salario_atual;
+	
+	int mes_ingresso, ano_ingresso;
+	int qtd_funcionarios;
 	
 	printf("Digite o mês corrente\n");
 	scanf("%d", &mes_corrente);
@@ -9,5 +17,22 @@ int main (void) {
 	printf("Digite o ano corrente\n");
 	scanf("%d", &ano_corrente);
 	
+	printf("Digite a quantidade de funcionários\n");
+	scanf("%d", &qtd_funcionarios);
+	
+	for (i = 1, i <= qtd_funcionarios, i++) {
+		printf("Digite a matricula do funcionario\n");
+		scanf("%d", &matricula_atual);
+		
+		printf("Digite o salário atual do funcionário\n");
+		scanf("%f", &salario_atual);
+		
+		printf("Digite o mês de ingresso do funcionário\n");
+		scanf("%d", &mes_ingresso);
+		
+		printf("Digite o ano de ingresso do funcionário\n");
+		scanf("%d", &ano_ingresso);
+		
+	}
 	
 }
